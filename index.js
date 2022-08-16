7810f3e86a503b9a6eec63d7dda729f03d8cc951
Kolonel1 <victor.munyao@student.moringaschool.com>
2022-08-16 10:47:49 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
-
+console.log(0.1 * 0.1);
 // Write your code here
+let num1=31;
+let num2=2;
+let multiply=num1*num2;
+
+let random=Math.round(Math.random());
+let num3 = 19;
+let num4= 5;
+let mod = num3 % num4;
+let max = Math.max(1,2,3,4,20);
